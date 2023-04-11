df06ebe7c567743473edcd44fc739b36204f66e6
xinyiqi <57293323+Xinyiqiw@users.noreply.github.com>
2023-04-12 00:55:16 +0900

    Needed libraries. First skeleton compose view. (#1)
    * empty commit
    
    * Delete .gradle/ from vcs.
    
    * Fix build.gradle.
    
    * Resolve libraries.
    
    * Launch default activity.

---
+++ app/src/main/java/com/dignicate/zero_2023_android/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.dignicate.zero_2023_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dignicate.zero_2023_android.ui.theme.Zero2023androidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Zero2023androidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Zero2023androidTheme {
+        Greeting("Android")
+    }
+}
