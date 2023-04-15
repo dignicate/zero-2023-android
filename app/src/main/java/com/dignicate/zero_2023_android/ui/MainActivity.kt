e6ca13ffd7da7260e5d2b33384faef47c3eb6f0e
Xinyiqi <shinichi_watanabe@dignicate.com>
2023-04-15 20:08:51 +0900

    navigation with hilt.

--- app/src/main/java/com/dignicate/zero_2023_android/ui/MainActivity.kt
+++ app/src/main/java/com/dignicate/zero_2023_android/ui/MainActivity.kt
@@ -3,16 +3,31 @@ package com.dignicate.zero_2023_android.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.dignicate.zero_2023_android.ui.theme.Zero2023androidTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * https://developer.android.com/jetpack/compose/navigation
+ * https://tabris.com/how-to-trigger-navigation-in-jetpack-compose-outside-of-composables/
+ * https://stackoverflow.com/questions/67180046/how-to-inject-a-viewmodel-into-a-composable-function-using-hilt-jetpack-compose
+ * https://proandroiddev.com/how-to-collect-flows-lifecycle-aware-in-jetpack-compose-babd53582d0b
+ * https://medium.com/@mikhail_zhalskiy/injecting-savedstatehandle-into-viewmodel-using-dagger-2-and-jetpack-compose-30b34df9ffd1
+ */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +39,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(
+                        modifier = Modifier,
+                        viewModel = hiltViewModel(),
+                    )
                 }
             }
         }
@@ -32,14 +50,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MainScreen(
+    modifier: Modifier,
+    viewModel: MainViewModel,
+) {
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Zero2023androidTheme {
-        Greeting("Android")
+private fun MainScreen(
+    modifier: Modifier,
+    destination: NavigationDestination,
+) {
+    val navController = rememberNavController()
+    LaunchedEffect(destination) {
+        if (navController.currentDestination?.route != destination.route) {
+            navController.navigate(destination.routeWithArgs)
+        }
+    }
+    Box(
+        modifier = modifier,
+    ) {
+        NavHost(navController, startDestination = ComposeScreen.Unknown.routeWithArgs) {
+            composable(
+                route = ComposeScreen.Main.BookList.ROUTE_WITH_ARGS,
+            ) {
+                // TODO:
+            }
+            composable(
+                route = ComposeScreen.Main.BookDetail.ROUTE_WITH_ARGS,
+                arguments = listOf(navArgument("id") { type = NavType.LongType })
+            ) {
+                val bookId = it.arguments!!.getLong("id")
+                // TODO:
+            }
+            composable(
+                route = ComposeScreen.Unknown.route,
+            ) {
+                // Empty
+            }
+        }
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//private fun MainScreen_Preview() {
+//    Zero2023androidTheme {
+//
+//    }
+//}
--- app/src/main/java/com/dignicate/zero_2023_android/ui/MainViewModel.kt
+++ app/src/main/java/com/dignicate/zero_2023_android/ui/MainViewModel.kt
@@ -8,5 +8,6 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val useCase: MainUseCase,
+    val navigator: ComposeNavigator,
 ) : ViewModel() {
 }
