5a9aca9fc4582d287ebd4f168a19d62fb7140c38
Xinyiqi <shinichi_watanabe@dignicate.com>
2023-04-16 22:36:30 +0900

    Book list view with preview.

--- app/src/main/java/com/dignicate/zero_2023_android/ui/BookListView.kt
+++ app/src/main/java/com/dignicate/zero_2023_android/ui/BookListView.kt
@@ -1,10 +1,22 @@
 package com.dignicate.zero_2023_android.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dignicate.zero_2023_android.ui.theme.Zero2023androidTheme
 
@@ -25,10 +37,56 @@ private fun BookListView(
     modifier: Modifier,
     data: BookListViewModel.Data?,
 ) {
-
+    val state = rememberLazyGridState()
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(90.dp),
+        state = state,
+    ) {
+        val items = data?.items ?: emptyList()
+        items(items.size) { index ->
+            val item = items[index]
+            BookListItemView(
+                modifier = modifier,
+                item = item,
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+private fun BookListItemView(
+    modifier: Modifier,
+    item: BookListViewModel.Data.Item,
+) {
+    Box(
+        modifier
+            .width(90.dp)
+            .height(128.dp)
+            .background(
+                color = MaterialTheme.colorScheme.background,
+                shape = RoundedCornerShape(6.dp)
+            )
+    ) {
+        Column(
+            modifier = modifier,
+        ) {
+            Text(
+                text = item.title,
+                color = MaterialTheme.colorScheme.primary,
+                style=  MaterialTheme.typography.titleMedium,
+                modifier = modifier,
+            )
+            Text(
+                text = item.author,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.labelSmall,
+                modifier = modifier,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = false)
 @Composable
 private fun BookListView_Preview() {
     Zero2023androidTheme {
@@ -45,6 +103,20 @@ private fun BookListView_Preview() {
                         author = "",
                     )
                 )
+            )
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+private fun BookListItemView_Preview() {
+    Zero2023androidTheme {
+        BookListItemView(
+            modifier = Modifier,
+            item = BookListViewModel.Data.Item(
+                title = "Title",
+                author = "Author",
             )
         )
     }
